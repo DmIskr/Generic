e3477a49be1ac54833a1c59a85f4a3f21b10ae60
DmIskr <diskrenok05@bk.ru>
2023-11-14 16:20:15 +0300

    generic2

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,12 @@
 
 public class Main {
     public static void main(String[] args) {
-        Generic<Integer, String, Double> twogenObj = new Generic<Integer, String, Double>(77, "abc", 2.5);
-        int num1 = twogenObj.getOb1();
-        String str2 = twogenObj.getOb2();
-        double num3 = twogenObj.getOb3();
+        Generic<Integer, String, Double> threegenObj = new Generic<Integer, String, Double>(77, "abc", 2.5);
+        int num1 = threegenObj.getOb1();
+        String str2 = threegenObj.getOb2();
+        double num3 = threegenObj.getOb3();
 
+        System.out.println(threegenObj);
 
-        System.out.println("Значение T: " + num1);
-        System.out.println("Значение V: " + str2);
-        System.out.println("Значение K " + num3);
     }
 }
